ab2986fa90eded04100d872c238a9829960ea822
Eunice Dickson <sunshinebae439@gmail.com>
2024-06-19 08:30:34 +0000

    Downloaded the resources(logos and images) for my home page

--- Jobizz/Jobizz/screens/HomePage.js
+++ Jobizz/Jobizz/screens/HomePage.js
@@ -1,36 +1,132 @@
-import { Text, View, Pressable, TextInput, Image, RootTagContext } from 'react-native';
+import { Text, View, Pressable, TextInput, Image, RootTagContext, ImageBackground, FlatList } from 'react-native';
 import styles from '../styles';
 //import { useState } from 'react';
 
 export default function Home({route}) {
-  //const profile = require
+  const jobs = require("../data/jobs.json");
+  const popularJobs = require("../data/popularJobs.json");
+  const profile = require("../assets/profile.png");
   const { username } = route.params;
+  const { email } = route.params;
+  const searchButton = require("../assets/search-button.png");
+  const searchDesign = require("../assets/button-design.png");
+  const redDot = require("../assets/red-dot.png");
+  const searchIcon = require("../assets/search.png");
+  const googleIcon = require("../assets/google-icon.png");
+  const facebookIcon = require("../assets/facebook-icon.png");
+  const burgerKing = require("../assets/burger-king.png");
+  const beats = require("../assets/beats.png");
 
-  const displayUsername = () => {
-    let text = "";
-    if({ username }) { 
-      text = {username}
-    } 
-    else 
-    { 
-      text = "Eric Atsu" 
-    }
-    return text;
+  const getUsername = () => {
+    return username || "Eric Atsu";
   }
 
-  const displayText = displayUsername();
+  const getEmail = () => {
+    return email || "eric@gmail.com";
+  }
+
+  const displayText = getUsername();
+  const displayMail = getEmail();
 
   return (
-    <View> 
-      <View>
+    <View style={styles.home}> 
+      <View style={styles.profile}>
         <View>
           <Text style={styles.usernameHeader}> {displayText} </Text>
-          <Text style={styles.emailHeader}>eric@gmail.com</Text>
+          <Text style={styles.emailHeader}> {displayMail} </Text>
         </View>
         <View>
-          
+          <ImageBackground 
+            source={profile} 
+            style={styles.profileImage}
+          >
+            <Image source={redDot} style={styles.redDot}/>
+          </ImageBackground>
         </View>
       </View>
+
+      <View style={styles.searchArea}>
+        <TextInput 
+          style={styles.search}
+          placeholder="Search a job or position"
+          placeholderTextColor="#95969D"
+          
+        />
+        <Pressable 
+          style={styles.searchButtonContainer}
+        > 
+          <ImageBackground source={searchButton} style={styles.searchButton}>
+            <Image source={searchDesign} style={styles.searchDesign}/>
+          </ImageBackground>
+        </Pressable>
+      </View>
+
+      <View style={styles.allJobs}>
+        <View style={styles.jobs}>
+          <Text style={styles.jobsHeader}>Featured Jobs</Text>
+          <Pressable >
+            <Text style={styles.viewJobs}>See all</Text>
+          </Pressable>
+        </View>
+        <FlatList 
+          data={jobs}
+          renderItem={({ item }) => {
+            const logo = parseInt(item.id) === 1 ? facebookIcon : googleIcon ; 
+            const backgroundColor = parseInt(item.id) === 1 ? "#2fa7ca" : "#04284A";
+
+            return (
+              <View style={[styles.jobCard, {backgroundColor: backgroundColor}]} key={item.id}>
+                <View style={styles.jobHeader}>
+                   <Image style={styles.jobLogo} source={logo}/>
+                  <View >
+                    <Text style={styles.jobTitle}>{item.jobTitle}</Text>
+                    <Text style={styles.jobCompany}>{item.Company}</Text>
+                  </View>
+                </View>
+                <View style={styles.jobFooter}>
+                  <Text style={styles.salary}> {item.Salary} </Text>
+                  <Text style={styles.salary}> {item.Location} </Text>
+                </View>
+              </View>
+            )
+          }}
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+        />
+      </View>
+
+      <View style={styles.popuplarJobs}>
+        <View style={styles.jobs}>
+            <Text style={styles.jobsHeader}>Popular Jobs</Text>
+            <Pressable >
+              <Text style={styles.viewJobs}>See all</Text>
+            </Pressable>
+          </View>
+        </View>
+
+       {/*} <FlatList 
+          data={popularJobs} 
+          renderItem={({item}) => {
+            let logo = "";
+            if(parseInt(item.id) === 1) {
+              logo = burgerKing;
+            } else if(parseInt(item.id) === 2) {
+              logo = beats;
+            } else {
+              logo = facebookIcon;
+            }
+
+            return (
+              <View style={styles.jobTab}>
+                <View style={styles.companyLogo}>
+                  <Image source={logo} />
+                </View>
+                <View></View>
+                <View></View>
+              </View>
+            )
+          }} 
+        /> */}
     </View>
   )
 }
--- Jobizz/Jobizz/styles.js
+++ Jobizz/Jobizz/styles.js
@@ -92,6 +92,138 @@ const styles = StyleSheet.create({
   errorMessage: {
     color: "red",
     marginBottom: 10,
+  }, 
+  home: {
+    paddingHorizontal: 20,
+    backgroundColor: "#FAFAFD",
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "space-between"
+  },
+  profile: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 30,
+    marginBottom: 30,
+  },
+  usernameHeader: {
+    fontWeight: "700",
+    fontSize: 24
+  }, 
+  emailHeader: {
+    fontWeight: "400",
+    fontSize: 20,
+    color: "#95969D",
+  },
+  profileImage: {
+    width: 58,
+    height: 58,
+  },
+  redDot: {
+    width: 5,
+    height: 5,
+    alignSelf: "flex-end",
+    marginTop: 5,
+    marginRight: 4,
+    borderColor: "#fff",
+    borderWidth: 3,
+    borderRadius: 30,
+    padding: 4,
+  },
+  searchArea: {
+    flexDirection: "row",
+    gap: 20,
+  },
+  search: {
+    width: 280,
+    height: 58,
+    borderRadius: 12,
+    borderColor: "transparent",
+    backgroundColor: "#F2F2F3",
+    borderWidth: 1,
+    padding: 15,
+  }, 
+  searchButton: {
+    height: 58,
+    width: 60,
+  }, 
+  searchDesign: {
+    width: 35,
+    heigth: 25,
+    alignSelf: "center",
+    marginTop: 15,
+  }, 
+  allJobs: {
+    flex: 1,
+    marginVertical: 40,
+    flexDirection: "column",
+  },
+  jobs: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  }, 
+  jobsHeader: {
+    color: "#0D0D26",
+    fontSize: 19,
+    fontWeight: "600",
+  }, 
+  viewJobs: {
+    color: "#95969D",
+    fontSize: 14,
+    fontWeight: "400",
+  }, 
+  jobCard: {
+    boderRadius: 24,
+    width: 310,
+    height: 210,
+    paddingHorizontal: 25,
+    paddingVertical: 25,
+    marginTop: 15,
+    borderRadius: 24,
+    flexDirection: "column",
+    justifyContent: "space-between",
+    marginRight: 10,
+    marginBottom: 20,
+  },
+  jobHeader: {
+    flexDirection: "row",
+    alignContent: "flex-start",
+    columnGap: 15,
+  }, 
+  jobLogo: {
+    width: 40,
+    height: 40, 
+    borderColor: "#FFFFFF",
+    borderRadius: 12, 
+    backgroundColor: "#FFFFFF",
+    borderWidth: 5,
+    padding: 0,
+  }, 
+  jobTitle: {
+    color: "#FFFFFF",
+    fontSize: 18, 
+    fontWeight: "600",
+    marginBottom: 5,
+  },
+  jobCompany: {
+    color: "#FFFFFF",
+    fontSize: 16,
+    fontWeight: "400",
+  }, 
+  jobFooter: {
+    flexDirection: "row",
+    justifyContent: "space-between"
+  }, 
+  salary: {
+    color: "#FFFFFF", 
+    fontWeight: "500",
+    fontSize: 15
+  }, 
+  popularJobs: {
+    marginTop: 0
+  }, 
+  jobTab: {
+    flexDirection: "row"
   }
 });
 
